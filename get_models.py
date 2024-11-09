78c93f1181b078f88b355d9b356731de9d170401
Mitchel555 <stoklasa.michal666@gmail.com>
2024-11-09 23:31:55 +0100

    modified:   get_models.py

--- get_models.py
+++ get_models.py
@@ -2,7 +2,7 @@ from elevenlabs import ElevenLabs
 from pprint import pprint
 
 client = ElevenLabs(
-    api_key="sk_99848a4e2760f019e60ad783be48531c932f919b231e2863",
+    api_key="xxx",
 )
 models = client.models.get_all()
 
